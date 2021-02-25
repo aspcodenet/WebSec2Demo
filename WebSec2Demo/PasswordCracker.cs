5c7df4e953051c117af5425b75a121ddddecc714
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-02-25 12:41:58 +0100

    after seminar

--- WebSec2Demo/PasswordCracker.cs
+++ WebSec2Demo/PasswordCracker.cs
@@ -1,4 +1,7 @@
-﻿namespace WebSec2Demo
+﻿using System;
+using System.IO;
+
+namespace WebSec2Demo
 {
     public class PasswordCracker
     {
@@ -16,8 +19,47 @@
         */
         public string Solve(string hashedPassword)
         {
+            Setup();
+            using (var f = File.OpenText("hashes.txt"))
+            {
+                while (true)
+                {
+                    var line = f.ReadLine();
+                    if (line == null) break;
+                    var parts = line.Split(':');
+                    var hashPart = parts[1];
+                    if (hashPart == hashedPassword)
+                    {
+                        return parts[0];
+                    }
+                }
+
+            }
+
+
             //Ok - nu ska vi KODA logik för att göra det omöjliga
             return null;
+        }
+
+        private void Setup()
+        {
+            if (!File.Exists("hashes.txt"))
+            {
+                using (var hashes = File.AppendText("hashes.txt"))
+                {
+                    using (var f = File.OpenText("commonPasswords.txt"))
+                    {
+                        while (true)
+                        {
+                            var line = f.ReadLine();
+                            if (line == null) break;
+                            hashes.WriteLine(line + ":" + PasswordHasher.CreateHash(line));
+                        }
+                    }
+
+                }
+            }
+
         }
     }
 }
